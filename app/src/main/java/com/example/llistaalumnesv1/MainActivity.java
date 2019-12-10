92ddb543ef5a8616fd2ffd1736219ab3f237be5d
Civerneitor <sergio_castilla_13@hotmail.com>
2019-12-10 13:14:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/llistaalumnesv1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.llistaalumnesv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_asistencia);
+    }
+}
