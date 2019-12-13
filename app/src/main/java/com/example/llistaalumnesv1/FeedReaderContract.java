1a3ca19cabc0760c2b088ea43f2272d5e8d7ba76
Civerneitor <sergio_castilla_13@hotmail.com>
2019-12-13 14:44:34 +0100

    SQL start2

--- app/src/main/java/com/example/llistaalumnesv1/FeedReaderContract.java
+++ app/src/main/java/com/example/llistaalumnesv1/FeedReaderContract.java
@@ -11,8 +11,9 @@ public class FeedReaderContract {
     public static class FeedEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "alumne";
-        public static final String COLUMN_NAME_ID = "id";
+        //public static final String _ID = "id";
         public static final String COLUMN_NAME_TITLE = "nom";
         public static final String COLUMN_NAME_SUBTITLE = "cognoms";
     }
+
 }
